5087fe6fbb4a336e53c09a946ad1dfbc4e93328d
Kolya <Walriblade@mail.ru>
2018-02-14 22:40:29 +0300

    Add content

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -3,6 +3,8 @@ import React from 'react'
 export const Contacts = props => (
   <div>
     <h1>Контакты</h1>
+    <p>Телефон: +7 (999) 999-99-99</p>
+    <button className="btn"><a href='https://www.vk.com/soniccreeper'>ВК</a></button>
   </div>
 )
 
